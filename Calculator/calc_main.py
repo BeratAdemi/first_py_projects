f688545d90bebfe5681e4c1f1062851e9db6ab37
BeratAdemi <berat.ademi1000@gmail.com>
2017-08-02 21:58:09 +0200

    bug fixes

--- Calculator/calc_main.py
+++ Calculator/calc_main.py
@@ -1,27 +1,9 @@
-# Importing my own help file
+# Importing necessary modules
 import help
 
 '''
-    FUNCTION
+    FUNCTIONS
 '''
-
-
-# Defining an introduction
-def start():
-    print 'Welcome! This is calculator offers the following functions:\n' \
-        '\n' \
-        'addition (add)\n' \
-        'subtraction (sub)\n' \
-        'multiplication (mul)\n' \
-        'division (div)\n' \
-        'modulo (mod) \n' \
-        '\n' \
-        'You call functions like this:\n' \
-        '1. Enter the function you want to use (add/sub/div/mul) \n' \
-        '2. Enter the first number \n' \
-        '3. Enter the second number \n' \
-        'If you need any further help call the function help.\n'
-    return
 
 
 # Function for addition and returning the result
@@ -106,9 +88,20 @@ def in_num2():
     EXECUTED CODE
 '''
 
-
-# Calling the function start
-start()
+# Introduction at program start
+print 'Welcome! This is calculator offers functions like:\n' \
+      '\n' \
+      'addition (add)\n' \
+      'subtraction (sub)\n' \
+      'multiplication (mul)\n' \
+      'division (div)\n' \
+      'modulo (mod) \n' \
+      '\n' \
+      'You use the functions like this:\n' \
+      '1. Enter the function you want to use (e.g. add/mul/mod) \n' \
+      '2. Enter the first number \n' \
+      '3. Enter the second number \n' \
+      'If you want to see a list of all supported functions call the function help.\n'
 
 # Setting run as True, run = False will stop the program
 run = True
